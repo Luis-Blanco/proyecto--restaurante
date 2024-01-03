3af869480464e415ee5692572f9490bbf3cc54a7
Luis <62902984+73025288L@users.noreply.github.com>
2024-01-02 22:45:29 -0400

    Creacion del slider para el menu

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 var btnBotonOpen = document.querySelector(".btn__menuOpen"),
    btnBotonClose = document.querySelector(".btn__menuClose"),
    menuResponsive = document.querySelector(".principal__menuBar"),
-   enlaces = document.querySelector("principal__enlaces");
+   enlaces = document.querySelector(".principal__enlaces");
 
 //CLIK ABRIR
 btnBotonOpen.addEventListener("click", function () {
@@ -11,12 +11,26 @@ btnBotonOpen.addEventListener("click", function () {
 });
 /* CLICK CERRAR */
 btnBotonClose.addEventListener("click", function () {
-   /* activar active para que a */
    menuResponsive.classList.remove("active");
 });
 
 /* CERRAR MENU CON ELEMENTOS DE ENLACE */
+
 enlaces.addEventListener("click", function () {
    menuResponsive.style.transitionDelay = "0.5s";
    menuResponsive.classList.remove("active");
+});
+
+/* SLIDER DE PRODUCTOS */
+
+var contenedor = document.querySelector(".menuProductos--slider"),
+   btnIzquierdo = document.querySelector(".menuProductos--btnIzquierda"),
+   btnDerecha = document.querySelector(".menuProductos--btnDerecha");
+
+btnDerecha.addEventListener("click", function () {
+   contenedor.scrollLeft += contenedor.offsetWidth;
+});
+
+btnIzquierdo.addEventListener("click", function () {
+   contenedor.scrollLeft -= contenedor.offsetWidth;
 });
